0bced2d11b495468e3060ceda4bddbc5511f4751
nguyenduyphuong90hy <nguyenduyphuong90hy@users.noreply.github.com>
2016-12-29 13:34:52 +0700

    Add files via upload

---
+++ Lazada Hunter Sale/Form1.Designer.cs
@@ -0,0 +1,367 @@
+﻿namespace Lazada_Hunter_Sale
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.stt = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Link = new System.Windows.Forms.DataGridViewLinkColumn();
+            this.Giacu = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.gia = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.sale = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(330, 196);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(158, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Tìm Kiếm";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(6, 85);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(307, 28);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 156);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(152, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Nhập Mức Giảm Giá";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(199, 156);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(145, 26);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(349, 159);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(23, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "%";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(602, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(196, 70);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(27, 200);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Số trang";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.Red;
+            this.label5.Location = new System.Drawing.Point(102, 200);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 20);
+            this.label5.TabIndex = 10;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.stt,
+            this.Link,
+            this.Giacu,
+            this.gia,
+            this.sale});
+            this.dataGridView1.Location = new System.Drawing.Point(4, 231);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(794, 374);
+            this.dataGridView1.TabIndex = 11;
+            this.toolTip1.SetToolTip(this.dataGridView1, "Để sắp sếp theo giá bạn hãy click vào tiêu đề của cột muốn sắp xếp");
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // stt
+            // 
+            this.stt.HeaderText = "Stt";
+            this.stt.Name = "stt";
+            this.stt.Width = 50;
+            // 
+            // Link
+            // 
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.25F);
+            this.Link.DefaultCellStyle = dataGridViewCellStyle4;
+            this.Link.HeaderText = "Link";
+            this.Link.Name = "Link";
+            this.Link.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Link.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            this.Link.Width = 250;
+            // 
+            // Giacu
+            // 
+            this.Giacu.HeaderText = "Giá Cũ";
+            this.Giacu.Name = "Giacu";
+            this.Giacu.Width = 150;
+            // 
+            // gia
+            // 
+            this.gia.HeaderText = "Giá Sau Khi Giảm";
+            this.gia.Name = "gia";
+            this.gia.Width = 200;
+            // 
+            // sale
+            // 
+            this.sale.HeaderText = "Sale";
+            this.sale.Name = "sale";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(494, 115);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(62, 26);
+            this.textBox2.TabIndex = 13;
+            this.textBox2.Text = "1";
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(387, 121);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(101, 20);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Tìm Từ Trang";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(572, 121);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(84, 20);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Đến Trang";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(662, 115);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(62, 26);
+            this.textBox3.TabIndex = 15;
+            this.textBox3.Text = "3";
+            this.textBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox3_KeyPress);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(4, 609);
+            this.progressBar1.Minimum = 1;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(802, 23);
+            this.progressBar1.Step = 1;
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 16;
+            this.progressBar1.Value = 1;
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.ToolTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(799, 24);
+            this.menuStrip1.TabIndex = 18;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(44, 20);
+            this.toolStripMenuItem1.Text = "Help";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(21, 25);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(171, 24);
+            this.radioButton1.TabIndex = 19;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Chọn Từ Danh Sách";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(21, 55);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(87, 24);
+            this.radioButton2.TabIndex = 20;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Tự Nhập";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Location = new System.Drawing.Point(28, 27);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(313, 114);
+            this.groupBox1.TabIndex = 21;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Hình thức danh mục";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(6, 88);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(307, 26);
+            this.textBox4.TabIndex = 22;
+            this.textBox4.Enter += new System.EventHandler(this.textBox4_Enter);
+            this.textBox4.Leave += new System.EventHandler(this.textBox4_Leave);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(799, 632);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Lazada Hunter Sale";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn stt;
+        private System.Windows.Forms.DataGridViewLinkColumn Link;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Giacu;
+        private System.Windows.Forms.DataGridViewTextBoxColumn gia;
+        private System.Windows.Forms.DataGridViewTextBoxColumn sale;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ Lazada Hunter Sale/Form1.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+using Fizzler.Systems.HtmlAgilityPack;
+using System.IO;
+
+namespace Lazada_Hunter_Sale
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+        InitializeComponent();
+        comboBox1.DisplayMember = "Text";
+        comboBox1.ValueMember = "Value";
+
+        var items = new[] {
+        new { Text = "Điện thoại Máy tính bảng", Value = "dien-thoai-may-tinh-bang" },
+        new { Text = "Điện thoại", Value = "dien-thoai-di-dong" },
+        new { Text = "Đồ gia dụng", Value = "do-gia-dung" },
+        new { Text = "Tủ lạnh", Value = "tu-lanh" }
+        };
+        comboBox1.DataSource = items;
+        textBox4.Hide();
+        radioButton1.Checked = true;
+        comboBox1.Show();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            HtmlWeb htmlweb = new HtmlWeb();
+            htmlweb.UserAgent = "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36";
+            String url = "http://www.lazada.vn/";
+            if (radioButton1.Checked)
+            {
+                url += comboBox1.SelectedValue.ToString();
+            }
+            else
+            {
+                if (textBox4.Text != "")
+                {
+                    url += textBox4.Text.ToString();
+                }
+                else
+                {
+                    MessageBox.Show("Bạn hãy nhập danh mục (Có thể nhấp Help để xem thông tin) !");
+                    url = "";
+
+                }
+            }
+            if(url !="" && url !=null)
+            {
+                try
+            {
+                if (textBox2.Text != null && textBox3.Text != null)
+                {
+                    dataGridView1.Rows.Clear();
+                    int count = 0;
+                    progressBar1.Minimum = 0;
+                    progressBar1.Value = 1;
+                    progressBar1.Step = 1;
+                    for (int i=Int32.Parse(textBox2.Text); i<Int32.Parse(textBox3.Text); i++)
+                    {
+                        HtmlAgilityPack.HtmlDocument htmldoc = htmlweb.Load(url+"/?page="+i);
+                        // Lấy page cuối
+                        var itemList = htmldoc.DocumentNode.QuerySelectorAll("div.c-paging__wrapper > a").ToList();
+                        var last = itemList[itemList.Count - 2];
+                        var link = last.Attributes["href"].Value;
+                        var text = last.InnerText;
+                        label5.Text = text.ToString();
+                        // Lấy giảm giá theo %
+                        var Listgiamgia = htmldoc.DocumentNode.QuerySelectorAll("div[data-qa-locator=product-item]").ToList();
+
+                        foreach (var item in Listgiamgia)
+                        {
+                            count++;
+                            // lấy link
+                            var linkNode = item.QuerySelector("a");
+                            var linkgiamgia = linkNode.Attributes["href"].Value;
+                            // Lấy giá cũ
+                            var getgiacu = item.QuerySelector("div.product-card__old-price");
+                            var giacu = "Not Detect";
+                            if (getgiacu!=null)
+                            {
+                               giacu = getgiacu.InnerText;
+                            }
+                           
+                            // Lấy Giá sau khi giảm SP
+                            var getgiasp = item.QuerySelector("div.product-card__price");
+                            var giasp = "Not Detect";
+                            if (getgiasp != null)
+                            {
+                                giasp = getgiasp.InnerText;
+                            }
+
+                            // Lấy sale
+                            var getsale = item.QuerySelector("div.price-block--grid > div.product-card__sale");
+                            var sale= "- 0%";
+                            if (getsale != null)
+                            {
+                                sale = getsale.InnerText;
+                            }
+                            // So sánh với giá trị Sale trong textbox
+                            if (textBox1.Text != null && textBox1.Text !="")
+                            {
+                               string justNumbers = new String(sale.Where(Char.IsDigit).ToArray());
+                               int sale_txt = Int32.Parse(textBox1.Text);
+                               if (Int32.Parse(justNumbers) >= sale_txt)
+                                {
+                                    dataGridView1.Rows.Add(new string[] { count.ToString(), linkgiamgia, giacu, giasp, sale });
+                                    DataGridViewLinkColumn dgvColDeletion = new DataGridViewLinkColumn();
+                                    dgvColDeletion.UseColumnTextForLinkValue = true;
+                                    dgvColDeletion.Text = "Delete";
+                                }
+                            }
+                            else
+                            {
+                                dataGridView1.Rows.Add(new string[] { count.ToString(), linkgiamgia, giacu, giasp, sale });
+                                //dataGridView1.Sort(dataGridView1.Columns[4], ListSortDirection.Descending);
+                               
+                            }
+                            progressBar1.Maximum = Listgiamgia.Count;
+                            progressBar1.PerformStep();
+                        }
+                    }//end for
+                  
+                }
+               
+            } //end try
+                
+            catch (Exception ex)
+            {
+                MessageBox.Show("Có lỗi trong quá trình xử lý, xin vui lòng kiểm tra lại");
+            }
+            }
+        }
+       
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char ch = e.KeyChar;
+            if (!Char.IsDigit(ch)) { e.Handled = true; }
+        }
+
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char ch = e.KeyChar;
+            if (!Char.IsDigit(ch)) { e.Handled = true; }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            string str = ((System.Windows.Forms.DataGridView)(sender)).Rows[e.RowIndex].Cells[e.ColumnIndex].Value.ToString();
+            System.Diagnostics.Process.Start(str);
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(@"C:\Program Files (x86)\Lazada Hunter Sale\Lazada Hunter Sale\help.txt"))
+            {
+                System.Diagnostics.Process.Start("notepad.exe", @"C:\Program Files (x86)\Lazada Hunter Sale\Lazada Hunter Sale\help.txt");
+            }
+            else
+            {
+                System.Diagnostics.Process.Start("notepad.exe", @"C:\Program Files\Lazada Hunter Sale\Lazada Hunter Sale\help.txt");
+            }
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                textBox4.Hide();
+                comboBox1.Show();
+            }
+            if (radioButton2.Checked)
+            {
+                textBox4.Show();
+                comboBox1.Hide();
+            }
+        }
+        private void textBox4_Enter(object sender, EventArgs e)
+        {
+            toolTip1.Show("Ví dụ:http://www.lazada.vn/dien-thoai-di-dong/ \n Các bạn Nhập vào là [dien-thoai-di-dong]", textBox4,999999);
+
+        }
+
+        private void textBox4_Leave(object sender, EventArgs e)
+        {
+            toolTip1.Hide(textBox4);
+        }
+    }
+}
